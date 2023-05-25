2b14af86c33986cf82924083db798f41c7c56edb
santoshchappidi11 <santoshchappidi3@gmail.com>
2023-05-25 11:21:09 +0530

    started javascript

---
+++ Day-14.js
@@ -0,0 +1,26 @@
+// DATATYPE:- string='', number=123, boolean=true/false
+
+
+// String----------------------------->
+// var firstName = 'Santosh';
+// console.log(firstName);
+// console.log(typeof(firstName));
+// var lastName = chappidi;
+// console.log(lastName)
+
+// Number----------------------------->
+// var someNumber = 345;
+// console.log(someNumber);
+// console.log(typeof(someNumber))
+// var numberString = '123';
+// console.log(numberString)
+
+
+// Boolean----------------------------->
+// var boolean = true;
+// console.log(boolean);
+// console.log(typeof(boolean))
+
+
+// var random = 'football';
+// console.log('This sport is called', random)
