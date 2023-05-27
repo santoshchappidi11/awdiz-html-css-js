257a6445b589d892512f9d9e44572ea02394dccb
santoshchappidi11 <santoshchappidi3@gmail.com>
2023-05-27 21:05:06 +0530

    javascript updates

---
+++ Day-15.js
@@ -0,0 +1,94 @@
+// Arithmetic Operators------------------------->
+// var number1 = 50;
+// var number2 = 45;
+
+// var addition = number1 + number2;
+// console.log(addition);
+
+// var subtraction = number1 - number2;
+// console.log(subtraction);
+
+// var multiplication = number1 * number2;
+// console.log(multiplication);
+
+// var division = number1 / number2;
+// console.log(division);
+
+// Increment Values---------------------------->
+// var i = 10;
+// i = i + 1;
+// console.log(i);
+// i++;
+// i++;
+// i++;
+// i++;
+// console.log(i);
+
+// comparison operator------------------------------>
+// var number = 100;
+// console.log(number > 100);
+// console.log(number < 100);
+// console.log(number == 100);
+
+// var number1 = 456;
+// var number2 = "456";
+// console.log(number1 == number2);
+// console.log(number1 === number2);
+
+// Logical operator-------------------------------------->
+// <-----------------&&-------------------->
+// var num1 = 55;
+// console.log(num1 > 60 && num1 < 80 && num1 >= 100);
+// console.log(num1 > 60 && num1 < 80 && num1 >= 55);
+// console.log(num1 > 45 && num1 < 80 && num1 >= 55);
+
+// <-----------||-------------------------->
+// var num2 = 60;
+// console.log(num2 > 60 || num2 < 80 || num >= 100);
+
+// Conditional Statement--------------------------------->
+
+// if(condition){
+//     console.log(execute the code)
+//     true
+// } else if(condition){
+//     console.log(execute the code)
+//     true
+// } else if(condition){
+//     console.log(execute the code)
+//     true
+// } else{
+//     console.log(execute the code)
+//     false
+// }
+
+// Q. check if the given number is 100 or 150 or 200
+// var number = 50;
+// var number = 140;
+// var number = 170;
+// var number = 240;
+
+// if (number < 100) {
+//   console.log("Number is lesser than 100");
+// } else if (number < 150) {
+//   console.log("Number is lesser than 150 and greater than 100");
+// } else if (number < 200) {
+//   console.log("Number is lesser than 200 and greater than 150");
+// } else {
+//   console.log("Number is greater than 100, 150, 200");
+// }
+
+// Q. check if the person is eligible for driving license------------>
+
+// var age = 18;
+// var age = 22;
+// var age = 82;
+// if (age == 18) {
+//   console.log("Person is eligible for Learning Licese....");
+// } else if (age > 18 && age < 80) {
+//   console.log("Person is eligible for Driving License.....");
+// } else {
+//   console.log(
+//     "Persone is not Eligible for Learning Licese and Driving License...."
+//   );
+// }
