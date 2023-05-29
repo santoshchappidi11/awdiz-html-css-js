ee9e2a5e0007dc32f4f8b065b957c72a94fd2f3f
santoshchappidi11 <santoshchappidi3@gmail.com>
2023-05-29 20:06:12 +0530

    javascript updates

---
+++ Day-16.js
@@ -0,0 +1,76 @@
+// Declaration--------------------------------->
+// var first;
+// console.log(first);
+
+// Assigning Values ----------------------------->
+// first = "santosh";
+// console.log(first);
+
+// Arrays-------------------------------------->
+// var Arr1 = ["santosh", "chappidi", 123, false, "random", 456];
+// console.log(Arr1[0]);
+// console.log(Arr1[3]);
+// console.log(Arr1[5]);
+
+// Loops-------------------------------------------------->
+// 1) <-----------------For Loop---------------->
+
+// for(initialization, condition, steps/increment/decrement){
+//     code to execute
+// }
+
+// Examples: ----------->
+
+// for (var i = 1; i <= 100; i++) {
+//   console.log(i);
+// }
+
+// for (var i = 1; i <= 100; i = i + 2) {
+//   console.log(i);
+// }
+
+// for (var i = 200; i <= 700; i++) {
+//   console.log(i);
+// }
+
+// for (var i = 50; i <= 230; i = i * 2) {
+//   console.log(i);
+// }
+
+// for (var i = 10; i <= 400; i = i + 6) {
+//   console.log(i);
+// }
+
+// for (var i = 20; i <= 500; i = i ** 2) {
+//   console.log(i);
+// }
+
+// for (var i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
+
+// for (var i = 10; i >= 1; i = i - 2) {
+//   console.log(i);
+// }
+
+// for (var i = 100; i >= 50; i = i - 5) {
+//   console.log(i);
+// }
+
+// var fruitsArray = ["Mango", "Grapes", "Banana", "Watermelon", "Guava"];
+
+// for (var i = 0; i <= fruitsArray.length - 1; i++) {
+//   console.log(fruitsArray[i]);
+// }
+
+// for (var i = 0; i <= fruitsArray.length - 1; i = i + 2) {
+//   console.log(fruitsArray[i]);
+// }
+
+// for (var i = 5 - 1; i >= 0; i--) {
+//   console.log(fruitsArray[i]);
+// }
+
+// for (var i = 5 - 1; i >= 0; i = i - 2) {
+//   console.log(fruitsArray[i]);
+// }
