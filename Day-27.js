305f7eba8b6bda79b3dfec265fd51340b6788df2
santoshchappidi11 <santoshchappidi3@gmail.com>
2023-06-17 23:13:01 +0530

    updates

---
+++ Day-27.js
@@ -0,0 +1,46 @@
+// Arrow Functions -------------------------------------->
+
+// const someFunction = (props) => console.log(props);
+// someFunction('hello')
+
+// Variables------------------------------>
+// Var
+// var myName = 'santosh';
+// myName = 'chappidi'
+// var myName = 'rakesh'
+// console.log(myName)
+// Ṛe-declaration and Re-Assinging Possible
+
+// Let
+// let myAge = 50;
+// myAge = 60
+// let myAge = 70;
+// console.log(myAge)
+// Re-declaration not possible but Re-assigning possible
+
+// Const
+// const myCity = "Mumbai";
+// myCity = "Hyderabad";
+// const myCity = "Banglore";
+// console.log(myCity);
+
+// Hoisting
+
+// console.log(myState)
+// myState = 'Maharashtra';
+// var myState;
+
+// Closures
+
+// function outerFunction() {
+//   var someName = "santosh";
+//   console.log("This is Outer Function");
+//   function innerFunction() {
+//     console.log("This is Inner Function", someName);
+//   }
+//   return innerFunction;
+// }
+
+// const inner = outerFunction();
+// console.log(inner)
+// console.log(inner());
